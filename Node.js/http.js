c9a315ab7ba7116618aaf2481f49e28758357548
willson <willsoncatacora1@gmail.com>
2024-01-17 09:53:27 -0400

    Metodos req y res

--- Node.js/http.js
+++ Node.js/http.js
@@ -1,7 +1,15 @@
 const http = require('http');
 
 const servidor = http.createServer((req, res) => {
-    console.log('Solicitud nueva');
+    // console.log('===> req (solicitud)');
+    // console.log(req.url);
+    // console.log(req.method);
+    // console.log(req.headers);
+
+    console.log('===> res (respuesta)');
+    // console.log(res.statusCode);
+    res.setHeader('content-type', 'application/json');
+    console.log(res.getHeaders());
     res.end('Hola, mundo');
 });
 
